00e30ab6075d00483981f116a439351ad743e1c6
Liam Curry <bill@curry.name>
2011-12-09 11:01:45 -0500

    Made init fire when loaded

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 'use strict';
 
-sp = getSpotifyApi(1);
+var sp = getSpotifyApi(1),
+    trackInfo = document.getElementById('track-info');
 
-exports.init = init;
 
-function init() {
-  console.log('init()');
+function updateTrackDetails() {
+  var playerTrackInfo = sp.trackPlayer.getNowPlayingTrack(),
+      infoHTML = '',
+      track;
+  if (playerTrackInfo == null) {
+    infoHTML = 'Nothing playing!';
+  } else {
+    track = playerTrackInfo.track;
+    console.log(playerTrackInfo);
+    console.log(track);
+    infoHTML = '<span id="track-title">' + track.name + '</span>' +
+      'on the album <span id="track-album">' + track.album.name + '</span>' +
+      'by <span id="track-artist">' + track.album.artist.name + '</span>';
+  }
+
+  trackInfo.innerHTML = infoHTML;
 }
+
+(function () {
+  console.log('init()');
+
+  updateTrackDetails();
+
+  sp.trackPlayer.addEventListener('playerStateChanged', function (event) {
+    // Only update the page if the track changed
+    if (event.data.curtrack == true) {
+        updatePageWithTrackDetails();
+    }
+  });
+})();
